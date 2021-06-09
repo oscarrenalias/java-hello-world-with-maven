21254fad6ff22fadb02915d031d30a3621e2236c
Oscar Renalias <oscar@renalias.net>
2021-06-10 00:57:36 +0300

    Moved code to the right package and fixed the workflow

---
+++ src/main/java/net/renalias/hello/Greeter.java
@@ -0,0 +1,7 @@
+package net.renalias.hello;
+
+public class Greeter {
+    public String sayHello() {
+        return "Hello, world!";
+    }
+}
--- src/main/java/net/renalias/hello/HelloWorld.java
+++ src/main/java/net/renalias/hello/HelloWorld.java
@@ -1,4 +1,4 @@
-package hello;
+package net.renalias.hello;
 
 import org.joda.time.LocalTime;
 
